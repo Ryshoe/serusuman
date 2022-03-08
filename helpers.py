4ded82441caaaedf54b583e29cdc598d4e009393
Ryshoe <angelonicopascua@yahoo.com>
2022-03-08 02:07:57 -0800

    Create method to read distances.csv into a list

--- helpers.py
+++ helpers.py
@@ -3,6 +3,36 @@ import csv
 from objects import Package
 
 
+def load_package_data():
+    with open('data/packages.csv') as data:
+        data = csv.reader(data, delimiter=',')
+        hash_table = HashTable()
+
+        for i in data:
+            package_id = int(i[0])
+            address = i[1]
+            city = i[2]
+            zip_code = i[4]
+            deadline = i[5]
+            weight = i[6]
+            package = Package(package_id, address, deadline, city, zip_code, weight)
+            hash_table.insert(package_id, package)
+
+        return hash_table
+
+
+def load_distance_data():
+    with open('data/distances.csv') as data:
+        data = csv.reader(data, delimiter=',')
+        distance_list = []
+
+        for i in data:
+            row = list(i)
+            distance_list.append(row)
+
+    return distance_list
+
+
 class HashTable:
     def __init__(self, initial_capacity=10):
         self.table = []
@@ -42,19 +72,3 @@ class HashTable:
     def bucket(self, key):
         bucket = hash(key) % len(self.table)
         return bucket
-
-    def load_package_data(self):
-        with open('data/packages.csv') as package_data:
-            package_data = csv.reader(package_data, delimiter=',')
-
-            for i in package_data:
-                package_id = int(i[0])
-                address = i[1]
-                city = i[2]
-                zip_code = i[4]
-                deadline = i[5]
-                weight = i[6]
-                package = Package(package_id, address, deadline, city, zip_code, weight)
-                self.insert(package_id, package)
-
-            return self
--- main.py
+++ main.py
@@ -6,13 +6,18 @@ import objects
 
 
 def main():
-    packages = helpers.HashTable.load_package_data(helpers.HashTable())
-    print(packages.table)
-    for i in range(len(packages.table)):
-        for j in range(len(packages.table[i])):
-            key = packages.table[i][j][0]
-            print(f'Package ID: {packages.search(key).id} '
-                  f'Address: {packages.search(key).address}')
+    # packages = helpers.load_package_data()
+    # print(packages.table)
+    # for i in range(len(packages.table)):
+    #     for j in range(len(packages.table[i])):
+    #         key = packages.table[i][j][0]
+    #         print(f'Package ID: {packages.search(key).id} '
+    #               f'Address: {packages.search(key).address}')
+
+    distances = helpers.load_distance_data()
+    print(distances)
+    for i in range(len(distances)):
+        print(distances[i])
 
 
 if __name__ == "__main__":
