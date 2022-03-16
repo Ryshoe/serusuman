113ab86b394756b6c008621714a0a6aec62ef941
Ryshoe <angelonicopascua@yahoo.com>
2022-03-15 22:41:11 -0700

    Update command-line interface

--- main.py
+++ main.py
@@ -5,11 +5,10 @@ import helpers
 import objects
 import sys
 import os
-import argparse
 
 
 def main():
-    # Create data objects to hold package info, distances, and addresses
+    # Create data objects to hold package and address data
     package_data = helpers.load_package_data()
     distance_data = helpers.load_distance_data()
     address_data = helpers.load_address_data()
@@ -24,20 +23,16 @@ def main():
     truck2 = helpers.deliver_packages(truck2, package_data, datetime.timedelta(hours=8))
     truck3 = helpers.deliver_packages(truck3, package_data, datetime.timedelta(hours=9, minutes=52, seconds=40))
 
-    # total_mileage = truck1.mileage + truck2.mileage + truck3.mileage
-    # print(f'Total mileage: {total_mileage}')
-    # helpers.package_status_by_id(package_data, 15)
-    # helpers.package_status_all(package_data, '8:00')
-
     # Main menu for CLI
     print('WGU Postal Service')
-    print('\n1) View package by id'
-          '\n2) View all packages'
-          '\n3) Exit')
+    print('\n1) View package details by id'
+          '\n2) View all packages by time'
+          '\n3) View truck mileage'
+          '\n4) Exit')
     val = int(input("\nPlease select a menu option: "))
 
     # Retrieve user input to select menu option
-    while val != 1 and val != 2 and val != 3:
+    while 1 > val > 4:
         val = int(input("Invalid syntax. Please select a menu option: "))
 
     if val == 1:
@@ -57,7 +52,16 @@ def main():
         input("\nPress ENTER to exit.")
         sys.exit(2)
     elif val == 3:
+        # Print truck details
+        os.system('cls')
+        print(f'Truck 1: {truck1.mileage} mi')
+        print(f'Truck 2: {truck2.mileage} mi')
+        print(f'Truck 3: {truck3.mileage} mi')
+        print(f'Total mileage: {truck1.mileage + truck2.mileage + truck3.mileage} mi')
+        input("\nPress ENTER to exit.")
         sys.exit(3)
+    elif val == 4:
+        sys.exit(4)
 
 
 if __name__ == "__main__":
